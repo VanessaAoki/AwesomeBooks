9a13f91b75b0d944136fbf3d16483224326047e2
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-08-09 18:10:23 -0300

    Add 'destroyBook' function and fix linters

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+/* eslint-disable prefer-const */
+
 let myBooks = [];
 
 function Book(title, author) {
   this.title = title;
   this.author = author;
+}
+
+function updateLocalStorage() {
+  localStorage.myBooks = JSON.stringify(myBooks);
+  getBooks();
+}
+
+function destroyBook(id) {
+  myBooks.splice(id, 1);
+  updateLocalStorage();
 }
